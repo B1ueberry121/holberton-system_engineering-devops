a9a7fa7e2273c3036093114a68ef4f9315f86496
B1ueberry121 <christianlaboy839@gmail.com>
2021-09-14 02:23:17 +0000

    upd

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 ''' Gathers data from an API '''
 import requests
-from sys import argv, exit
+import sys
+
+
+def get_employ(emp_id):
+    """ Gets and shows employee completion rate of to-dos """
+    url = 'https://jsonplaceholder.typicode.com/'
+    emp_r = requests.get(url + 'users/{}'.format(emp_id))
+    emp_dict = emp_r.json()
+    emp_todo = requests.get(url + 'todos/', params={'userId': emp_id}).json()
+    todo_comp = 0
+    comp_task = []
+    for item in emp_todo:
+        if item.get('completed'):
+            todo_comp += 1
+            comp_task.append(item.get('title'))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(emp_dict.get("name"), todo_comp, len(emp_todo)))
+    for item in comp_task:
+        print("\t {}".format(item))
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Usage: {} <employeeID>".format(argv[0]))
-        exit()
+    get_employ(sys.argv[1])
 
-    userID = argv[1]
-    site = 'https://jsonplaceholder.typicode.com/'
-    to_dos = requests.get(site + 'todos?userId={}'.format(userId)).json()
-    done = [t['title'] for t in to_dos if t['completed'] id True]
-    user = requests.get(site + 'users?id={}'.format(userId)).json()
-    name = user[0].get('name')
-    print('Employee {} is done with tasks({}/{}):'.format(name, len(done),
-                                                          len(to_dos)))
-    for task in done:
-        print('\t {}'.format(task))
+
