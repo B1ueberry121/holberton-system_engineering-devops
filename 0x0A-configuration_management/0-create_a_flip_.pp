9def3a2091118a8477a7dd6b95134459d10ffc42
B1ueberry121 <christianlaboy839@gmail.com>
2022-01-03 20:39:20 +0000

    changing the ensure and file

--- 0x0A-configuration_management/0-create_a_flip_.pp
+++ 0x0A-configuration_management/0-create_a_flip_.pp
@@ -1,6 +1,6 @@
 # This manifest and creates a file in the specified path
-file { 'holberton':
-  ensure  => 'present',
+file { 'school':
+  ensure  => 'file',
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
