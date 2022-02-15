3bfabfd4910cd5ed461af778f308218a9f04579c
B1ueberry121 <christianlaboy839@gmail.com>
2022-02-15 19:07:49 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' prints the titles of the first 10 hot posts '''
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)\
+               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+               Safari/537.36"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        response = response.json().get('data').get('children')
+
+        for post in response:
+            title = post.get('data').get('title')
+            print(title)
+    else:
+        print("None")
